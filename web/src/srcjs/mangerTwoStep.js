2594badceff097ce09624e28d880b20a630534c4
jxchen <jxchenvip@163.com>
2017-06-27 10:40:09 +0800

    update

--- web/src/srcjs/mangerTwoStep.js
+++ web/src/srcjs/mangerTwoStep.js
@@ -2,7 +2,6 @@
  * [showDetail 查看详情]
  * @type {Object}
  */
-const Viewer = require('../libs/viewer.min.js');
 
 const showDetail = {
     wrap: $('#schemeList'),
@@ -14,15 +13,17 @@ const showDetail = {
         var cname = 'schemeList__item--active',
             $parent = $open.parents('li');
         $parent.toggleClass(cname);
+        this.lookPic($parent);
     },
-    lookPic() {
-        if (this.viewer) {
-            this.viewer = null;
-        }
-        this.viewer = new Viewer(document.getElementById('schemeList'), {
-            toolbar: false,
-            navbar: false
-        });
+    lookPic($parent) {
+        // if (this.viewer) {
+        //     this.viewer = null;
+        // }
+        // var dom = $parent.find('.infoDetail').get(0);
+        // this.viewer = new Viewer(dom, {
+        //     toolbar: false,
+        //     navbar: false
+        // });
     },
     /**
      * [init 初始化]
@@ -30,15 +31,25 @@ const showDetail = {
     init() {
         var that = this;
         this.viewer = null;
-        
-        
+
+        // $(document).on('click', 'img', function() {
+        //     if(!$(this).data('data-original')) {
+        //         return false;
+        //     }
+        // })
+        // 
+        console.log($('img[data-original]').length)
+
+
+
         // 阻止一些图片可预览
-        this.wrap.on('click', '.infoSimple__open', function() {
+        this.wrap.on('click', '.infoSimple__txtBox--poa', function() {
             that.showDetail($(this));
-            that.lookPic()
             return false;
         })
 
     }
 };
 showDetail.init();
+
+$('#schemeList').viewer();
