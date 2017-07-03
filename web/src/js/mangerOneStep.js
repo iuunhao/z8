4142fd5cc02d358d8a3a4e9899e04ef6bb35d17c
iuunhao <html.5@foxmail.com>
2017-07-03 18:20:14 +0800

    update

--- web/src/js/mangerOneStep.js
+++ web/src/js/mangerOneStep.js
@@ -73,7 +73,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -94,7 +94,7 @@ function showTips(str) {
 
 /***/ }),
 
-/***/ 6:
+/***/ 7:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
