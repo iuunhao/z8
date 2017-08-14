dd8ef4c58af8408d0bc5bc670deb62b233d163ad
jxchen <jxchenvip@163.com>
2017-08-14 13:43:02 +0800

    update

--- src/js/find_category.js
+++ src/js/find_category.js
@@ -1,23 +1,3 @@
-require(['zepto'], function($) {
-
-    var Tabs = {
-        requestData: function(e) {
-            var $button = $(e.target),
-                sname = this.btnName,
-                cname = this.conName,
-                index = $button.index();
-            $button.addClass(sname).siblings('a').removeClass(sname);
-            this.cons.addClass(cname).eq(index).removeClass(cname);
-        },
-        init: function() {
-            this.conName = 'none';
-            this.btnName = 'active';
-            this.wrap = $('#ctabs');
-            this.btns = this.wrap.find('.classTabs__left');
-            this.cons = this.wrap.find('.classTabs__right');
-
-            this.btns.on('click', this.requestData.bind(this));
-        }
-    };
-    Tabs.init();
-})
+require(['zepto', 'categoryPop'], function($, category) {
+    console.log(category)
+});
