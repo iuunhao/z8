4977750e49da1c61a66442222be9ccae4d8379bf
iuunhao <html.5@foxmail.com>
2017-08-21 10:24:24 +0800

    register

--- src/js/lib/common.js
+++
@@ -1,7 +0,0 @@
-define('common', ['jquery', 'showTips'], function($, showTips) {
-    /** @type {Object} [alert, confirm] */
-    $.mine = {};
-    $.mine.alert = showTips.alert;
-    $.mine.confirm = showTips.confirm;
-    return $;
-})
